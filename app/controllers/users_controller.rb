39718e8386bb7157a08d5eaaca60b5b988537f35
pulsydoctor39@yahoo.co.jp <ec2-user@ip-172-31-44-216.us-east-2.compute.internal>
2019-12-25 08:20:52 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger インスタンス変数を定義した直後にこのメソッドが実行される
   end
+
+  def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入する
+  end
+  
+  def create
+    @user = User.new(user_params) #(params[:user])同じ
+    if @user.save # 保存に成功した場合は、ここに記述した処理が実行される
+    flash[:success] = "新規作成に成功しました。"
+    redirect_to @user #redirect_to user_url(@user)同じ
+    else
+      render 'new'
+    end
+  end
+  
+private
+
+  def user_params #Rails4.0以降では、セキュリティを高めるStrong Parametersと呼ばれるテクニックで対策することが標準化された
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end  
 end
